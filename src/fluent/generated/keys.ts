008a0becc9a6dd497d1df78b0d6ee76201a079eb
System Administrator <admin@example.com>
2026-03-21 22:12:36 +0100

    init

---
+++ src/fluent/generated/keys.ts
@@ -0,0 +1,32 @@
+import '@servicenow/sdk/global'
+
+declare global {
+    namespace Now {
+        namespace Internal {
+            interface Keys extends KeysRegistry {
+                explicit: {
+                    bom_json: {
+                        table: 'sys_module'
+                        id: '9985b9ae263041eda823c6d60093def5'
+                    }
+                    br0: {
+                        table: 'sys_script'
+                        id: '9aa197dd694b473fadc1d2237a34c800'
+                    }
+                    cs0: {
+                        table: 'sys_script_client'
+                        id: 'a3d6bfe849e44cf5988d9b834e417569'
+                    }
+                    package_json: {
+                        table: 'sys_module'
+                        id: 'aa86a0fd230a42f0aa07901e8270840e'
+                    }
+                    src_server_script_ts: {
+                        table: 'sys_module'
+                        id: 'a77ea02e53e74d129377350709015195'
+                    }
+                }
+            }
+        }
+    }
+}
